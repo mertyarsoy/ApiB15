e197c64c94587fe305fdf0ec41c16acab49f0b74
mertyarsoy <mertyarsoy@gmail.com>
2023-04-16 21:18:01 -0400

    Initial Commit

--- src/test/java/post/PetStore.java
+++ src/test/java/post/PetStore.java
@@ -14,10 +14,25 @@ public class PetStore {
         RestAssured.baseURI = "https://petstore.swagger.io";
         RestAssured.basePath = "v2/pet";
 
+        //POST https://petstore.swagger.io/v2/pet
+        //GET https://petstore.swagger.io/v2/pet//252177
+
+        /*
+        1. Created a pet
+        2. Validated POST call response body and status code
+        3. Sent GET request with newly created petId
+        4. Validated GET call response body and status code
+         */
+
         String petName = "diablo",petStatus = "unavailable"; long petId = 252177;
 
-        Response response = RestAssured.given().accept(ContentType.JSON).contentType(ContentType.JSON) // bc it's post request
-                .body(PayloadUtils.getPetPayload(252177,"diablo","unavailable")).when().post().then().statusCode(200).extract().response();
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .contentType(ContentType.JSON) // bc it's post request
+                .body(PayloadUtils.getPetPayload(252177,"diablo","unavailable"))
+                .when()
+                .post()
+                .then()
+                .statusCode(200).extract().response();
 
         PetPojo parsedResponse = response.as(PetPojo.class);
 
@@ -25,5 +40,16 @@ public class PetStore {
         Assert.assertEquals(petName,parsedResponse.getName());
         Assert.assertEquals(petStatus,parsedResponse.getStatus());
 
+        Response response2 = RestAssured.given().accept(ContentType.JSON)
+                .when()
+                .get("https://petstore.swagger.io/v2/pet/"+petId)
+                .then()
+                .statusCode(200).extract().response();
+
+        PetPojo parsedGetResponse = response2.as(PetPojo.class);
+
+        Assert.assertEquals(petId,parsedGetResponse.getId());
+        Assert.assertEquals(petName,parsedGetResponse.getName());
+        Assert.assertEquals(petStatus,parsedGetResponse.getStatus());
     }
 }
